a7e6d9e99e05e22f9598a8fe42da15f6e1bb9f0b
KicksCOMS <152629969+KicksCOMS@users.noreply.github.com>
2023-12-16 02:44:22 +0100

    Update antyspam.lua

--- antyspam.lua
+++ antyspam.lua
@@ -1,73 +1,39 @@
-﻿-- Pierwotny właściciel i twórca kodu: ZbyK; Zbigniew Płachciński, zbigniewplachcinski@gmail.com
--- Zakaz używania / edycji kodu bez wcześniejszej zgody ze strony właściciela
--- Zakaz wykorzystywania całego lub fragmentów kodu
--- W przypadku złamania powyzszych zasad sprawa zostanie skierowana do sądu pod zarzutem: Naruszenia majątkowych praw autorskich
--- Pierwotnie pełne prawo do używania tego kodu posiada tylko ZbyK i tylko on ma prawo pozwolić uzytkować lub zarządać trwałego usunięcia kodu lub jego fragmentów
+import sys 
+import os 
+import time 
+import ctypes 
 
+def set_console_title(title): 
+   ctypes.windll.kernel32.SetConsoleTitleW(title) 
 
-anty = {
-	message={},
-	timer={},
-	
-}
+def loading_animation(): 
+    animation = "|/-\\" 
+    for i in range(20): 
+        time.sleep(0.1) 
+        sys.stdout.write("\r" + "ŁADOWANIE... " + animation[i % len(animation)])  # Poprawiona linijka
+        sys.stdout.flush() 
 
-function antyspam(index,plr,time,msg,timemsg,ge)
-	if index and plr then
-		if time and tonumber(time) then 
+def fake_error(): 
+    set_console_title("Spoofer ładowanie...") 
+    loading_animation() 
+    time.sleep(3) 
+    os.system('cls') 
+    set_console_title("Coprything: Projekt Averos") 
+    print ("###################################") 
+    print ("#         Autor: .jvsh.           #") 
+    print ("#            Discord:             #") 
+    print ("#   https://discord.gg/4X5XjDxMf8 #") 
+    print ("###################################") 
+    time.sleep(5) 
+    os.system('cls') 
+    set_console_title("Podrabianie serialu...") 
+    loading_animation() 
+    time.sleep(1) 
+    os.system('cls') 
+    print ('[+] Podrabianie serialu...') 
+    time.sleep(3) 
+    os.system('cls') 
+    set_console_title("Podrobiony serial") 
+    print("ZROBIONE!") 
 
-			if plr and isElement(plr) then 
-				if getElementData(plr,"ACL") then 
-					return 0
-				end
-				plr = getPlayerName(plr) 
-			end
-			
-			if not anty.timer[index] then
-				anty.timer[index]={}
-			end
-			
-			
-			if msg and timemsg and tonumber(timemsg) then
-			
-				if not anty.message[index] then 
-					anty.message[index]={}
-				end
-				
-				if anty.message[index][msg] and anty.message[index][msg][1] == plr and getTickCount() - anty.message[index][msg][2] < timemsg then 
-					if ge~=true then
-						anty.message[index][msg] = {plr,getTickCount()} 
-					end
-					return 1 
-				else
-					anty.message[index][msg] = {plr,getTickCount()} 
-				end
-			end
-			
-			
-			if anty.timer[index][plr] and getTickCount() - anty.timer[index][plr] < time then
-				if ge~=true	then
-					anty.timer[index][plr] = getTickCount()
-				end
-				return 2
-			else
-				anty.timer[index][plr] = getTickCount()
-			end
-		
-			
-		end
-	end
-	return 0
-end
-
-
-
-if localPlayer then 
-
-	-- addCommandHandler("antyspam",function(_,index,msg)
-		-- outputChatBox(antyspam(index,localPlayer,5000,msg,1000))
-	-- end)
-
-	fileDelete("antyspam.lua") 
-end
-
-
+fake_error()
